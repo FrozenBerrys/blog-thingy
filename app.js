332370330faa7e0f25635ac9a1b16234e51709f6
boko <bokosandesu@gmail.com>
2024-07-12 03:14:50 +0800

    file upload

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000 ;
+
+
+app.use(express.static('public'));
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+//defaults to rendering main.ejs instead of searching for an index.ejs in views 
+app.set('view engine', 'ejs');
+
+
+
+app.use('/', require('./server/routes/main')); 
+// any request made to the root URL (/) or any subpath under / will be handled by the router exported from ./server/routes/main
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`);
+})
---
+++ server/routes/main.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Routes
+router.get('', (req,res)=>{
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple blog created with NodeJS, MongoDB and Express"
+    } // sending locals. as data to dynamically load into the layout 
+    res.render("index", { locals });
+})
+
+
+router.get('/about', (req,res)=>{
+    res.render("about");
+});
+
+router.get('/contact', (req,res)=>{
+    res.render("contact");
+});
+
+module.exports = router;
