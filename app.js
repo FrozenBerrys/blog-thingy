9da4df2bc89f107b097dce4aa68af90a2b7de171
boko <bokosandesu@gmail.com>
2024-07-13 04:17:17 +0800

    just need admin page now

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000 ;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use(express.static('public'));
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post.js');
 
 
 //Routes
-router.get('', (req,res)=>{
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple blog created with NodeJS, MongoDB and Express"
-    } // sending locals. as data to dynamically load into the layout 
-    res.render("index", { locals });
-})
+//GET HOME
+router.get('', async (req,res)=>{
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple blog created with NodeJS, MongoDB and Express"
+        }
 
+            let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        // Count is deprecated - please use countDocuments
+        // const count = await Post.count();
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+        });
+    } catch (error) {
+        console.log("Error");
+    }
+    
+
+
+
+
+
+
+
+
+});
 
 router.get('/about', (req,res)=>{
     res.render("about");
@@ -20,4 +55,95 @@ router.get('/contact', (req,res)=>{
     res.render("contact");
 });
 
+//GET Post :id
+router.get('/post/:id', async (req,res)=>{
+    try {
+        let slug = req.params.id;
+
+
+
+        const data = await Post.findById({ _id: slug})
+        // sending locals. as data to dynamically load into the layout 
+
+        const locals = {
+            title: data.title ,
+            //description: "Simple blog created with NodeJS, MongoDB and Express" MAYBE INCORPORATE INTO DATABASE
+        }
+
+        res.render("post", { locals, data });
+    } catch (error) {
+        console.log("Error");
+    }
+});
+
+// GET Post: searchterm
+router.post('/search', async (req,res)=>{
+    try {
+        const locals = {
+            title: "Search",
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+        const data = await Post.find({
+            $or: [
+              { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+              { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+            ]
+          });
+
+
+
+        //res.render("search", { locals, data });
+        res.render("search", {
+            data,
+            locals,
+          });
+    } catch (error) {
+        console.log("Error");
+    }
+});
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+
+// function insertPostData (){
+//     Post.insertMany([
+//         {
+//             title: "Dead Internet Theory",
+//             body: "Is all of this webdev stuff worth it in the end? Doesn't matter as long as I enjoy it right?"
+
+//         },
+//     ])
+// }
+// insertPostData();
+
+
+
+
+
+//WITHOUT PAGINATION
+// router.get('', async (req,res)=>{
+//     const locals = {
+//         title: "NodeJs Blog",
+//         description: "Simple blog created with NodeJS, MongoDB and Express"
+//     }
+    
+//     try {
+//         const data = await Post.find()
+//         // sending locals. as data to dynamically load into the layout 
+//         res.render("index", { locals, data });
+//     } catch (error) {
+//         console.log("Error");
+//     }
+// });
